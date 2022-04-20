9a451f084c430b7553136cd0cfe8775d0526b013
saikiranreddy <saikiranreddy@gmail.com>
2022-04-20 22:57:07 +0530

    [kiran]-Create Stack

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Stacks And Queues Program");
+
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+
+            linkedListStack.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
